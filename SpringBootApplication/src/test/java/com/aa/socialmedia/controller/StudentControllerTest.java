6a0f3dc8cd7b351a047b60cc744d36a8cbcf87d3
Suyesh <Suyesh.Deshmukh@aa.com>
2022-07-14 21:04:35 +0530

    Changes for Junit

--- SpringBootApplication/src/test/java/com/aa/socialmedia/controller/StudentControllerTest.java
+++ SpringBootApplication/src/test/java/com/aa/socialmedia/controller/StudentControllerTest.java
@@ -2,10 +2,14 @@ package com.aa.socialmedia.controller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.aa.socialmedia.model.Student;
+import com.aa.socialmedia.service.StudentService;
 
 @SpringBootTest
 class StudentControllerTest {
@@ -13,12 +17,20 @@ class StudentControllerTest {
 	@Autowired
 	private StudentController controller;
 
+	@MockBean
+	private StudentService studentService;
+
 	@Test
 	void test() {
-		Student student = controller.getstudentByID(2);
+		Student st = new Student();
+		st.setStudentName("Abcd");
+		st.setStudentEmail("xml@gmail.com");
+		st.setStudentBranch("Computer");
+		Mockito.when(studentService.getStudentByID(ArgumentMatchers.any())).thenReturn(st);
+		Student student = controller.getstudentByID(10);
 		Assertions.assertNotNull(student);
 
-		Assertions.assertEquals("Abc", student.getStudentName());
+		Assertions.assertEquals("Abcd", student.getStudentName());
 	}
 
 }
